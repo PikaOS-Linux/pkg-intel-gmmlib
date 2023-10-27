694bf7583402d8a1bc911df19ba8cf8d1ea15624
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2023-10-27 15:43:26 +0300

    first commit

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ DEBIAN_FRONTEND=noninteractive
 git clone https://github.com/intel/gmmlib ./intel-gmmlib -b intel-gmmlib-22.3.12
 cp -rvf ./debian ./intel-gmmlib/
 cd ./intel-gmmlib/
+for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
 # Get build deps
 apt-get build-dep ./ -y
